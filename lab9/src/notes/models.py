6edeb8249f93f3c0b4adb6d03fb373617f0956d7
Michael Galperin <mgalp.connect@gmail.com>
2017-12-15 19:32:25 +0300

    Added NoteAdmin model

--- lab9/src/notes/models.py
+++ lab9/src/notes/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from datetime import timedelta
+from django.utils import timezone
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
