d803ee3f6156cc848dbebfa72d4db40d7f136aa2
Michael Galperin <mgalp.connect@gmail.com>
2017-12-16 00:36:35 +0300

    Migrated notes model

--- lab9/src/notes/models.py
+++ lab9/src/notes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from datetime import timedelta
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
+    owner = models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE, default=1)
 
     def was_published_recently(self):
         now = timezone.now()
