5b1c6325cb63cf3a6c29047149662a30495e6786
Michael Galperin <mgalp.connect@gmail.com>
2017-12-16 00:52:44 +0300

    Added NoteCreate

--- lab9/src/notes/urls.py
+++ lab9/src/notes/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from .views import NoteList, NoteDetail, NoteCreate
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<note_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', NoteList.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', NoteDetail.as_view(), name='detail'),
+    url(r'^new/$', NoteCreate.as_view(), name='create'),
 ]
--- lab9/src/notes/views.py
+++ lab9/src/notes/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView, CreateView
+from django.utils import timezone
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Note
+from .forms import NoteForm
 
 @login_required
 def index(request):
@@ -16,3 +22,35 @@ def index(request):
 def detail(request, note_id):
      note = get_object_or_404(Note, pk=note_id)
      return render(request, 'notes/detail.html', {'note': note})
+
+class NoteList(ListView):
+    paginate_by = 5
+    template_name = 'notes/index.html'
+    context_object_name = 'latest_note_list'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(NoteList, self).dispatch(*args, **kwargs)
+
+    def get_queryset(self):
+        return Note.objects.filter(owner=self.request.user).order_by('-pub_date')
+
+
+class NoteDetail(DetailView):
+    model = Note
+    template_name = 'notes/detail.html'
+    context_object_name = 'note'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(NoteDetail, self).dispatch(*args, **kwargs)
+
+class NoteCreate(LoginRequiredMixin, CreateView):
+    form_class = NoteForm
+    template_name = 'notes/form.html'
+    success_url = reverse_lazy('notes:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.pub_date = timezone.now()
+        return super(NoteCreate, self).form_valid(form)
